01ff8c4266f10f7856ae2cb703ca0140c10c067f
= <=>
2023-04-15 10:29:01 +0200

    I finished first step - sorting, ordering and searching by the key

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,10 @@
+package com.company.enroller.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController{
+
+}
